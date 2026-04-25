04f40c08b359594b6189f2eac22dbeecb9d04a8c
Waleed Ahmed <ahmedwaleed2856@gmail.com>
2026-04-25 16:21:17 +0500

    Implement palindrome check in palendrome.py

--- palendrome.py
+++ palendrome.py
@@ -1,1 +1,11 @@
+word = input("Enter the word")
+
+save_word = ""
+
+for chr in word:
+
+    save_word = chr + save_word
+if word == save_word:print(f"The word is palendrome: {save_word}")
+
+else:print(f"Not palendrome:{save_word}")
 
